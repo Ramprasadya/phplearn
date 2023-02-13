38ccb9740954c72535112037cb105c747fe327d4
Ramprasadya <yadavramprasad563@gmail.com>
2023-02-13 18:59:01 +0530

    insert data into database

--- insertdata.php
+++ insertdata.php
@@ -1,5 +1,40 @@
 <?php 
 
+echo "inserting data into databaase";
+echo "<br>";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "tabledb";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if (!$conn) {
+	 die("Connection faild".mysqli_error());
+}else{
+	echo "Connection complete";
+}
+
+// using variable for saving the data ;
+
+$name = "Ronit";
+$age = "34";
+$gender = "male";
+
+// inserting data into database ;
+
+
+$sql = "INSERT INTO   employee( name, age, gender) VALUES ( '$name', '$age', '$gender')";
+
+$result =mysqli_query($conn,$sql);
+
+if($result){
+	echo "New record added successfully !!";
+}
+else{
+	echo "Unable to insert data" .$sql. "<br>" .mysqli_error($conn);
+};
 
 
 ?>
